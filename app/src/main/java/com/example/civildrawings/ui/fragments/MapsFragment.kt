c2286354a327e8b53eecd91a5ce36e362abbf9ee
muhammad.inamulhaq <muhammad.inamulhaq@ezono.com>
2022-04-30 21:27:19 +0200

    Location from fused location provider

--- app/src/main/java/com/example/civildrawings/ui/fragments/MapsFragment.kt
+++ app/src/main/java/com/example/civildrawings/ui/fragments/MapsFragment.kt
@@ -1,25 +1,33 @@
 package com.example.civildrawings.ui.fragments
 
+import android.Manifest
 import android.annotation.SuppressLint
-import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.location.LocationRequest
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.civildrawings.R
 import com.example.civildrawings.databinding.FragmentMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.Task
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
@@ -32,7 +40,7 @@ class MapsFragment : Fragment() {
     private lateinit var locationListner: LocationListener
     private val locationPermissionCode = 101
     private val DEFAULT_ZOOM = 20F
-
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
 
     @SuppressLint("MissingPermission")
@@ -40,7 +48,7 @@ class MapsFragment : Fragment() {
         mMap = googleMap
         Log.i(tag, "testing - maps ready")
 
-        val jena = LatLng(50.928610570437456, 11.597427035737676)
+        val jena = LatLng(33.602361841189, 72.98659947140648)
         mMap.addMarker(MarkerOptions().position(jena).title("Marker in Jena"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(jena, DEFAULT_ZOOM))
 
@@ -52,6 +60,8 @@ class MapsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+
 
         binding = FragmentMapsBinding.inflate(layoutInflater, container, false)
         mapsFragmentViewModel = ViewModelProvider(this)[MapsFragmentViewModel::class.java]
@@ -80,11 +90,53 @@ class MapsFragment : Fragment() {
 
         binding.btGetCurrentLocation.setOnClickListener {
             mapsFragmentViewModel.getCurrentLocation()
-            locationManager = requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 10f, locationListner)
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10, 10f, locationListner)
+//            locationManager =
+//                requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+//            locationManager.requestLocationUpdates(
+//                LocationManager.GPS_PROVIDER,
+//                10,
+//                10f,
+//                locationListner
+//            )
+//            locationManager.requestLocationUpdates(
+//                LocationManager.NETWORK_PROVIDER,
+//                10,
+//                10f,
+//                locationListner
+//            )
+
+            if (ActivityCompat.checkSelfPermission(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    locationPermissionCode
+                )
+            }
+            else{
+                fusedLocationClient.lastLocation
+                    .addOnSuccessListener {
+                        if (it == null) {
+                            Log.i(tag, "testing - location is null")
+                        } else {
+                            var loc = LatLng(it.latitude, it.longitude)
+                            mMap.addMarker(MarkerOptions().position(loc).title("Current Location"))
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, DEFAULT_ZOOM))
+                            Log.i(tag,"testing - longitude: ${it.longitude.toString()} - latitude: ${it.latitude.toString()}")
+                        }
+                    }
+            }
         }
 
     }
+
+
+
 
 }
